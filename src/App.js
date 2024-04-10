9681d8a496ce1109cdfc4fe0c04e7b7bb7fdd7e4
BluTargaryan <opeyemiyolo@gmail.com>
2024-04-10 11:15:13 -0400

    comp creation and impl

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Navbar/Navbar.js'
 import Landing from './Landing_Page/LandingPage.js';
+import SignUp from './SignUp/SignUp.js';
+import Login from './Login/Login.js';
 import './App.css';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
           <Navbar/>
               <Routes>
               <Route path="/" element={<Landing/>}/>
+              <Route path="/signup" element={<SignUp/>}/>
+              <Route path="/login" element={<Login/>}/>
               </Routes>
             
         </BrowserRouter>
